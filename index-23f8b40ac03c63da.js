f4cb463263f08e3fd7e8b66e2f4ee831b0537734
simonsan <simonsan@users.noreply.github.com>
2021-10-26 05:55:10 +0000

    deploy: fe975202b6525b1da6d971e60b040ddf8f8f195d

--- index-23f8b40ac03c63da.js
+++ index-23f8b40ac03c63da.js
@@ -267,7 +267,7 @@ async function load(module, imports) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('index-8240ada026b88b0b_bg.wasm', import.meta.url);
+        input = new URL('index-23f8b40ac03c63da_bg.wasm', import.meta.url);
     }
     const imports = {};
     imports.wbg = {};
